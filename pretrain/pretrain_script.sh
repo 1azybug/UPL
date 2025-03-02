5be7dccc6c1b5ead7795fbad358a866a46506458
Meteor <1762204162@qq.com>
2025-03-02 23:02:26 +0800

    Start!

---
+++ pretrain/pretrain_script.sh
@@ -0,0 +1,15 @@
+
+python pre_prepare_data.py --work_dir 'test'
+python ./pre_trainer.py --work_dir 'test' --port 14529
+python ./pre_evaluator.py --work_dir 'test' --batch_size 1
+
+
+
+
+
+
+
+
+
+
+
