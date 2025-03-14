7982d6c8c13550a09e304078f2e0417b703767a9
Meteor <1762204162@qq.com>
2025-03-14 23:06:04 +0800

    一些小改动

--- pretrain/pretrain_script.sh
+++ pretrain/pretrain_script.sh
@@ -1,7 +1,10 @@
 
-python pre_prepare_data.py --work_dir '../experiment/example'
-python ./pre_trainer.py --work_dir '../experiment/example' --port 14529
-python ./pre_evaluator.py --work_dir '../experiment/example' --batch_size 1
+python pre_prepare_data.py --work_dir '../experiment/no-pe'
+python ./pre_trainer.py --work_dir '../experiment/no-pe' --port 14525
+python ./pre_evaluator.py --work_dir '../experiment/no-pe' --batch_size 1
+
+python ./pre_trainer.py --work_dir '../experiment/you-pe' --port 14526
+python ./pre_evaluator.py --work_dir '../experiment/you-pe' --batch_size 1
 
 
 
--- sft/sft_script.sh
+++ sft/sft_script.sh
@@ -1,8 +1,16 @@
 
 
-python instruction_prepare_data.py --work_dir '../experiment/example'
-python ./instruction_trainer.py --work_dir '../experiment/example' --port 14525
-python ./instruction_evaluator.py --work_dir '../experiment/example' --batch_size 1
+
+
+
+# 运行前记得改一下learning_rate=0.00005,use_ae_loss=False
+python instruction_prepare_data.py --work_dir  '../experiment/no-pe'
+python ./instruction_trainer.py --work_dir  '../experiment/no-pe' --port 14527
+python ./instruction_evaluator.py --work_dir  '../experiment/no-pe' --batch_size 1
+
+
+python ./instruction_trainer.py --work_dir  '../experiment/you-pe' --port 14528
+python ./instruction_evaluator.py --work_dir  '../experiment/you-pe' --batch_size 1
 
 
 
