1dafb97416d60f002ee1c66ec10b96943a9bb15a
1azybug <1079934359@qq.com>
2025-03-20 21:08:30 +0800

    add config

--- pretrain/pretrain_script.sh
+++ pretrain/pretrain_script.sh
@@ -7,17 +7,14 @@
 # python ./pre_evaluator.py --work_dir '../experiment/you-pe' --batch_size 1
 
 
-python pre_prepare_data.py --work_dir '../experiment/debug/quick'
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/debug/quick' --port 14574
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/debug/quick' --batch_size 1
+python pre_prepare_data.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --port 14574
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --batch_size 1
 
 
 
-
-
-
-
-
-
+python pre_prepare_data.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_UPL'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_UPL' --port 14574
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_UPL' --batch_size 1
 
 
