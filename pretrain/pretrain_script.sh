5b3a98a1c576a9ab37aab855e00f9096226a550a
1azybug <1079934359@qq.com>
2025-03-23 23:25:29 +0800

    nothing

--- pretrain/pretrain_script.sh
+++ pretrain/pretrain_script.sh
@@ -15,4 +15,12 @@
 
 # python pre_prepare_data.py --work_dir '../experiment/debug/500xquick'
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/debug/500xquick' --port 14574
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/debug/500xquick' --batch_size 1
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/debug/500xquick' --batch_size 1
+
+python pre_prepare_data.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_DPL'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_DPL' --port 14574
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_DPL' --batch_size 1
+
+python pre_prepare_data.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_UPL'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_UPL' --port 14574
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_UPL' --batch_size 1
--- sft/sft_script.sh
+++ sft/sft_script.sh
@@ -18,10 +18,30 @@
 # python ../util/evaluate_iid.py --work_dir '../experiment/debug/quick'
 
 
-python instruction_prepare_data.py --work_dir  '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_trainer.py --work_dir  '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --port 14527
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_evaluator.py --work_dir  '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --batch_size 1
-python ../util/evaluate_ood.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
-python ../util/evaluate_iid.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
+# python instruction_prepare_data.py --work_dir  '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_trainer.py --work_dir  '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --port 14527
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_evaluator.py --work_dir  '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --batch_size 1
+# python ../util/evaluate_ood.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
+# python ../util/evaluate_iid.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
 
+# python instruction_prepare_data.py --work_dir  '../experiment/debug/500xquick'
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_trainer.py --work_dir  '../experiment/debug/500xquick' --port 14527
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_evaluator.py --work_dir  '../experiment/debug/500xquick' --batch_size 1
+# python ../util/evaluate_ood.py --work_dir '../experiment/debug/500xquick'
+# python ../util/evaluate_iid.py --work_dir '../experiment/debug/500xquick'
+
+
+
+python instruction_prepare_data.py --work_dir  '../experiment/local_experiment/500x_Llama-3.2-1B_DPL'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_trainer.py --work_dir  '../experiment/local_experiment/500x_Llama-3.2-1B_DPL' --port 14527
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_evaluator.py --work_dir  '../experiment/local_experiment/500x_Llama-3.2-1B_DPL' --batch_size 1
+python ../util/evaluate_ood.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_DPL'
+python ../util/evaluate_iid.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_DPL'
+
+
+python instruction_prepare_data.py --work_dir  '../experiment/local_experiment/500x_Llama-3.2-1B_UPL'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_trainer.py --work_dir  '../experiment/local_experiment/500x_Llama-3.2-1B_UPL' --port 14527
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_evaluator.py --work_dir  '../experiment/local_experiment/500x_Llama-3.2-1B_UPL' --batch_size 1
+python ../util/evaluate_ood.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_UPL'
+python ../util/evaluate_iid.py --work_dir '../experiment/local_experiment/500x_Llama-3.2-1B_UPL'
 
